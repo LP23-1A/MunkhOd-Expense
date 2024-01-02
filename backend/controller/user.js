4b833fe85655746fe03940fd1944bb2026193be8
Munkh-Od Batsukh <23LP5760@G303-CC0146.local>
2024-01-02 11:12:41 +0800

    expense

---
+++ backend/controller/user.js
@@ -0,0 +1,62 @@
+import { pool } from "../db.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const queryText = `SELECT * FROM users`;
+    const response = await pool.query(queryText);
+
+    res.send(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getOneUser = async (req, res) => {
+  const { id, name, email } = req.body;
+
+  try {
+    const queryText = `SELECT * FROM users WHERE name='${name}' AND email='${email}'`;
+    const response = await pool.query(queryText);
+    res.send(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createUser = async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const queryText = `SELECT * FROM users WHERE name='${name}' AND email='${email}'`;
+    const response = await pool.query(queryText);
+    res.send(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { name, email, id } = req.body;
+
+  try {
+    const queryText = `DELETE FROM users WHERE (name = '${name}' AND email = '${email}') OR id = '${id}'`;
+    await pool.query(queryText);
+    res.send("ok");
+  } catch (error) {
+    res.send("error").end();
+    console.error(error);
+  }
+};
+
+export const updateUser = async (req, response) => {
+  const { name, email, id } = req.body;
+
+  try {
+    const queryText = `UPDATE users SET name = '${name}', email = '${email}' WHERE id = '${id}'`;
+    await pool.query(queryText);
+    response.send("updated");
+  } catch (error) {
+    response.send("error").end();
+    console.error(error);
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import bp from "body-parser";
+import { pool } from "./db.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bp.json());
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
+});
+
+app.post("/createTable", async (_, res) => {
+  try {
+    const tableQueryText = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL
+    )`;
+    await pool.query(tableQueryText);
+    res.send("ok");
+  } catch (error) {
+    console.error(error);
+  }
+});
