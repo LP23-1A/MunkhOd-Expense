cf2847750a129d1a74a14c0e6beb15c66ad8d0fe
Munkh-Od Batsukh <23LP5760@G303-CC0146.local>
2024-01-03 12:34:28 +0800

    expense

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,9 +1,24 @@
-import Login from "@/components/Login";
+import LoginLogo1 from "@/components/icons/LoginLogo1";
+import LoginLogo2 from "@/components/icons/LoginLogo2";
 
 export default function login() {
   return (
     <main>
-      <Login />
+      <div className="flex justify-center">
+        <div className="w-[732px] h-[1024px] inline-flex flex-col items-center gap-[40px]">
+          <div className="flex p-[5.405px] items-center gap-[9.459px]">
+            <LoginLogo1 />
+            <LoginLogo2 />
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-[24px] font-semibold">Welcome Back</h1>
+            <p className="text-[16px] font-normal">
+              Welcome back, Please enter your details
+            </p>
+          </div>
+        </div>
+        <div className="w-[708px] h-[1024px] bg-[#0166FF]"></div>
+      </div>
     </main>
   );
 }
