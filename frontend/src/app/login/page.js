904964acdcafcc233f81041faea534cdb8fad2fc
Munkh-Od Batsukh <23LP5760@G303-CC0146.local>
2024-01-04 12:36:01 +0800

    expense

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,11 +1,16 @@
 import LoginLogo1 from "@/components/icons/LoginLogo1";
 import LoginLogo2 from "@/components/icons/LoginLogo2";
+import axios from "axios";
+const s = async () => {
+  let data = await axios.get("http://localhost:8001");
+  console.log(data);
+};
 
 export default function login() {
   return (
     <main>
-      <div className="flex justify-center">
-        <div className="w-[732px] h-[1024px] inline-flex flex-col items-center gap-[40px]">
+      <div className="flex w-full">
+        <div className="w-1/2 flex-col items-center gap-[40px] flex justify-center">
           <div className="flex p-[5.405px] items-center gap-[9.459px]">
             <LoginLogo1 />
             <LoginLogo2 />
@@ -16,8 +21,27 @@ export default function login() {
               Welcome back, Please enter your details
             </p>
           </div>
+          <div className="flex flex-col gap-4">
+            <input
+              className="p-[16px] border rounded-lg bg-[#F3F4F6] border-[#D1D5DB] w-[384px] text-[#A3A3A3]"
+              type="text"
+              placeholder="Email"
+            />
+            <input
+              className="p-[16px] border rounded-lg bg-[#F3F4F6] border-[#D1D5DB] w-[384px] text-[#A3A3A3]"
+              type="text"
+              placeholder="Password"
+            />
+            <button className="flex items-center justify-center w-[384px] h-[48px] rounded-[20px] bg-[#0166FF] text-[#FFFFFF]">
+              Log in
+            </button>
+          </div>
+          <div className="flex gap-2">
+            <button>Don't have account?</button>
+            <button className="text-[#0166FF]">Sign Up</button>
+          </div>
         </div>
-        <div className="w-[708px] h-[1024px] bg-[#0166FF]"></div>
+        <div className="w-1/2 h-screen bg-[#0166FF]"></div>
       </div>
     </main>
   );
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,3 @@
-import Login from "@/components/Login";
-
 export default function Home() {
-  return (
-    <main>
-      <Login />
-    </main>
-  );
+  return <main></main>;
 }
